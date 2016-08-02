150d889bb4ee8eddadd7ee20175d9d54a51f37ef
Raghu Udiyar <raghusiddarth@gmail.com>
2016-08-02 23:56:49 +0530

    Simple tool for spring BCryptPasswordEncoder

---
+++ README.md
@@ -0,0 +1,13 @@
+SpringPasswordEncoder
+===
+A simple tool to generate spring [BCryptPasswordEncoder](https://docs.spring.io/spring-security/site/docs/current/apidocs/org/springframework/security/crypto/bcrypt/BCryptPasswordEncoder.html) hashes, and also to verify existing hashes. This is just to generate hashes for [Kylin](http://kylin.apache.org/docs/howto/howto_ldap_and_sso.html) where the admin password is unfortunately [hardcorded](https://github.com/KylinOLAP/Kylin/blob/ab0118647200f7704db79e4b2a76e4588616c27d/server/src/main/resources/kylinSecurity.xml), but I guess can be used for other purposes as well.
+
+## Build
+
+$ mvn package
+
+## Usage
+
+$ java -jar target/spring-password-0.1.0.jar
+ -e : Encode password into hash (default: false)
+ -v : Decode password into hash (default: true)
---
+++ src/main/java/SpringPasswordEncoder/SpringPasswordEncoder.java
@@ -0,0 +1,59 @@
+package SpringPasswordEncoder;
+
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import org.kohsuke.args4j.CmdLineParser;
+import org.kohsuke.args4j.CmdLineException;
+import org.kohsuke.args4j.Option;
+import org.kohsuke.args4j.Argument;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class SpringPasswordEncoder {
+
+  @Option(name="-e", usage="Encode password into hash")
+  private boolean encodeFlag;
+
+  @Option(name="-v", usage="Decode password into hash")
+  private boolean verifyFlag;
+
+  @Argument
+  private List<String> arguments = new ArrayList<String>();
+
+  public static void main(String[] args) {
+    new SpringPasswordEncoder().doMain(args);
+  }
+
+  public void doMain(String[] args) {
+
+    CmdLineParser parser = new CmdLineParser(this);
+
+    try {
+      parser.parseArgument(args);
+
+      if ( arguments.isEmpty() )
+        throw new CmdLineException(parser, "No options passed");
+
+    } catch ( CmdLineException e ) {
+      parser.printUsage(System.err);
+    }
+
+    org.springframework.security.crypto.password.PasswordEncoder encoder
+    = new org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder();
+
+    if ( encodeFlag ) {
+      System.out.println("Enter password to encrypt : ");
+      String inputpass = System.console().readLine();
+      System.out.println("Password hash is : ");
+      System.out.println(encoder.encode(inputpass));
+    } else if ( verifyFlag ) {
+      System.out.println("Enter hash : ");
+      String hash = System.console().readLine();
+      System.out.println("Enter expected password : ");
+      String pass = System.console().readLine();
+      System.out.println(encoder.matches(pass, hash));
+    }
+  }
+}
